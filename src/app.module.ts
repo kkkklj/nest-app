78724b5f295e357771e57db1e22633a5705f6912
KaiL <819103524@qq.com>
2023-11-15 17:42:43 +0800

    延迟接口

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DebugController } from './debug/debug.controller';
+
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController,DebugController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/debug/debug.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('/debug')
+export class DebugController {
+  @Get('/wait5')
+  getWait() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+       resolve({
+          msg:'hello',
+          code:200
+        }) 
+      }, 5000);
+    }) 
+  }
+}
